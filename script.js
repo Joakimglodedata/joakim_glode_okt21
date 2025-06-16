d9a625c4dbaee40332df625ff546af40966466f0
Joakimglodedata <joakim.glodedata@gmail.com>
2025-06-16 10:06:29 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const ø = (selector) => document.querySelector(selector);
+const æ = (selector) => console.log(selector);
+// æ(ø(".flexCenterRow"))
+// console.log(document.querySelector(".flexCenterRow"))
